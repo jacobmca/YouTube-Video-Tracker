cf5223b64ffeadc4d5420c5ae8250b8699483e97
Jacob McAuliffe <64445624+jacobmca@users.noreply.github.com>
2025-01-09 09:05:48 -0800

    Building out MongoDB database.

--- client/YouTube-Video-Tracker/src/main.jsx
+++ client/YouTube-Video-Tracker/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Reach from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import client from './apolloClient';
+import { ApolloProvider } from '@apollo/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+);
